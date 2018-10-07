113ee33670a22c95f0be04b7384b2934b531feac
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 18:29:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+  let variable = [];
+  for(let i = 0; i < arrayMusicians; i++){
+    variable.push(arrayMusicians[i] + ' plays ' + arrayInstruments[i]);
+  }
+  return variable;
+}
+
+function johnLennonFacts(array) {
+  let n = array.length - 1;
+  while(i >= 0) {
+    array[i] += '!!!';
+    n--;
+  }
+  return '!!!';
+}
